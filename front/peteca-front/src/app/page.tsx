79887edc86534747b12c00460081bde3e1f044cf
Matheus <matheushamada@live.com>
2024-01-25 15:54:28 -0300

    Renomeação de arquivo

--- front/peteca-front/src/app/page.tsx
+++ front/peteca-front/src/app/page.tsx
@@ -1,5 +1,5 @@
 import "bootstrap/dist/css/bootstrap.min.css";
-import TemplateSistema from "./components/templates/TemplateSistema";
+import TemplateSistema from "./components/templates/SystemTemplate";
 import { config } from "@fortawesome/fontawesome-svg-core";
 import "@fortawesome/fontawesome-svg-core/styles.css";
 config.autoAddCss = false;
