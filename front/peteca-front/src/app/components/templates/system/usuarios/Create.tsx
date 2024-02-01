9c9809373c7fe9399a945419885b499cc6b67e4e
Matheus <matheushamada@live.com>
2024-01-31 21:03:08 -0300

    Icone e botão de alterar foto (não funcional ainda)

--- front/peteca-front/src/app/components/templates/system/usuarios/Create.tsx
+++ front/peteca-front/src/app/components/templates/system/usuarios/Create.tsx
@@ -1,4 +1,4 @@
-import { faChevronLeft } from "@fortawesome/free-solid-svg-icons";
+import { faChevronLeft, faUser, faUserCircle } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 export default function Create() {
@@ -12,11 +12,14 @@ export default function Create() {
           <FontAwesomeIcon icon={faChevronLeft} size="2x" /> Voltar
         </a>
         <h1 className="text-warning">Cadastrar usuário</h1>
-        <div className="row">
-          <div className="col-4">
-
+        <div className="row mt-3">
+          <div className="col-md-4 justify-content-center align-items-center">
+            <div className="row">
+              <FontAwesomeIcon icon={faUserCircle} size="10x" />
+              <button className="btn btn-primary btn-sm rounded-5 col-8 mt-3">Alterar foto</button>
+            </div>
           </div>
-          <div className="col-4">
+          <div className="col-md-4">
             <div>
               <label>Nome</label>
               <input type="text" className="form-control-sm form-control" />
@@ -38,7 +41,7 @@ export default function Create() {
               <input type="text" className="form-control-sm form-control" />
             </div>
           </div>
-          <div className="col-4"></div>
+          <div className="col-md-4"></div>
         </div>
       </div>
     </>
