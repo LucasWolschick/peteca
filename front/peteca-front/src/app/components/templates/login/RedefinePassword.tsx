8e36e043aa43c7d77b2be1915899872e6ac4df14
Matheus <matheushamada@live.com>
2024-02-02 10:35:43 -0300

    Tela de RedefinePassword ajustada

--- front/peteca-front/src/app/components/templates/login/RedefinePassword.tsx
+++ front/peteca-front/src/app/components/templates/login/RedefinePassword.tsx
@@ -1,41 +1,40 @@
+import { faLock } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+
 export default function RedefinePassword() {
   return (
     <>
-      <div>
-        <h3 className="text-warning d-flex justify-content-center align-self-center">
+      <div className="row justify-content-center align-self-center">
+        <h3 className="text-warning text-center mb-3">
           REDEFINIR SENHA
         </h3>
-      </div>
-      <div className="row justify-content-center align-self-center">
-        <div className="col-6">
-          <div className="row justify-content-center align-self-center m-2">
-            <div className="col-8">
-              <h5 className="text-light d-flex justify-content-left">
-                INSIRA A NOVA SENHA
-              </h5>
-              <input
-                type="text"
-                placeholder="nova senha"
-                className="form-control form-control-md p-3 mb-3"
-              />
+        <div className="col-md-6 col-12">
+          <div className="row justify-content-around align-self-center m-2">
+            <div className="col-md-8 col-12">
+              <label className="text-light d-flex justify-content-left">
+                Insira a nova senha
+              </label>
+              <div className="input-group input-group-lg mb-3">
+                <span className="input-group-text"><FontAwesomeIcon icon={faLock} /></span>
+                <input type="password" className="form-control" placeholder="**********" />
+              </div>
             </div>
-            <div className="col-8">
-              <h5 className="text-light d-flex justify-content-left">
-                CONFIRME A NOVA SENHA
-              </h5>
-              <input
-                type="text"
-                placeholder="confirmar nova senha"
-                className="form-control form-control-md p-3"
-              />
-            </div>  
+            <div className="col-md-8 col-12">
+              <label className="text-light d-flex justify-content-left">
+                Confirme sua senha
+              </label>
+              <div className="input-group input-group-lg mb-3">
+                <span className="input-group-text"><FontAwesomeIcon icon={faLock} /></span>
+                <input type="password" className="form-control" placeholder="**********" />
+              </div>
+            </div>
           </div>
           <div className="row justify-content-center align-self-center m-3">
-            <button className="btn btn-primary btn-md rounded-4 p-2 mb-2 col-8">
-              Entrar
+            <button className="btn btn-primary btn-md rounded-4 p-2 col-md-8 col-12">
+              Redefinir senha
             </button>
-            <button className="btn btn-danger btn-md rounded-4 p-2 col-8">
-              Esqueci minha senha
+            <button className="btn btn-danger btn-md rounded-4 p-2 mt-3 col-md-8 col-12">
+              Cancelar
             </button>
           </div>
         </div>
