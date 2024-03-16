b9a2c9e4f4c164182423e8138b12ec0d7a570bd6
Marcos <marcosoliveira159@outlook.com>
2024-03-16 16:20:07 -0300

    Dropbox para filtro e campo de texto implementados (Front)

--- front/src/app/components/templates/system/estoque/Index.tsx
+++ front/src/app/components/templates/system/estoque/Index.tsx
@@ -2,13 +2,37 @@ import DialogCreateItem from "@/app/components/items/system/DialogCreateItem";
 import DialogEditItem from "@/app/components/items/system/DialogEditItem";
 import Item from "@/app/components/items/system/Item";
 import Title from "@/app/components/items/system/Title";
+import { useState } from "react";
 
 export default function Index() {
+
+    const [searchTerm, setSearchTerm] = useState('');
+    const [filterOption, setFilterOption] = useState('');  
+
     return (
         <>
             <div className="container-fluid">
                 <Title title="Estoque" />
+                <div className="mb-3">
+                    <input
+                        type="text"
+                        value={searchTerm}
+                        onChange={e => setSearchTerm(e.target.value)}
+                        placeholder="Pesquisar..."
+                    />
 
+                    <select
+                        value={filterOption}
+                        onChange={e => setFilterOption(e.target.value)}
+                    >
+                        <option value="">Filtrar por...</option>
+                        <option value="option1">Data</option>
+                        <option value="option2">Quantidade</option>
+                        <option value="option3">Local</option>
+                        <option value="option4">Nome</option>
+                        {/* Add more options as needed */}
+                    </select>
+                </div>
                 <div className="table-responsive bg-white">
                     <table className="table table-sm table-striped">
                         <thead className="sticky-top">
