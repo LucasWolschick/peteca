b851f712af08edea05a47172215756c2c07f8661
Lucas Wolschick <6264382+LucasWolschick@users.noreply.github.com>
2024-03-16 16:20:10 -0300

    Adiciona logging básico

--- back/src/logger.ts
+++ back/src/logger.ts
@@ -1,19 +1,27 @@
-import * as winston from 'winston';
+import * as winston from "winston";
 
 const logger = winston.createLogger({
-    level: 'info',
-    format: winston.format.json(),
-    defaultMeta: { service: 'user-service' },
-    transports: [
-        new winston.transports.File({ filename: 'error.log', level: 'error' }),
-        new winston.transports.File({ filename: 'combined.log' }),
-    ]
-})
+  level: "info",
+  format: winston.format.combine(
+    winston.format.timestamp(),
+    winston.format.json()
+  ),
+  transports: [
+    new winston.transports.File({ filename: "error.log", level: "error" }),
+    new winston.transports.File({ filename: "combined.log" }),
+  ],
+});
 
-if (process.env.NODE_ENV !== 'production') {
-    logger.add(new winston.transports.Console({
-        format: winston.format.simple()
-    }))
+if (process.env.NODE_ENV !== "production") {
+  logger.add(
+    new winston.transports.Console({
+      format: winston.format.combine(
+        winston.format.colorize(),
+        winston.format.timestamp(),
+        winston.format.simple()
+      ),
+    })
+  );
 }
 
 export default logger;
--- back/src/server.ts
+++ back/src/server.ts
@@ -5,9 +5,10 @@ import * as express from "express";
 import userController from "./controller/UserController";
 import permissionsController from "./controller/PermissionsController";
 import itemController from "./controller/ItemController"; // Importação do ItemController
-import { errorHandler } from "./errors";
+import { NotFoundError, errorHandler } from "./errors";
 import { PermissionsRepository } from "./repository/PermissionsRepository";
 import { PrismaClient } from "@prisma/client";
+import logger from "./logger";
 
 const app = express();
 const PORT = 8080;
@@ -18,20 +19,29 @@ dotenv.config();
 // Base
 app.use(express.json());
 app.use(
- cors({
+  cors({
     origin: true,
     methods: ["GET", "POST", "PUT", "DELETE"],
- })
+  })
 );
+app.use((req, res, next) => {
+  logger.info(`${req.method} ${req.url} ${req.ip}`);
+  next();
+});
 
 // Routers
 app.use("/api/user", userController);
 app.use("/api/permissions", permissionsController);
 app.use("/api/items", itemController); // Rota para os itens
 
+// 404 handler
+app.use((req, res, next) => {
+  throw new NotFoundError("Não encontrado");
+});
+
 // Gerenciamento de erros
 app.use(errorHandler);
 
 app.listen(PORT, () => {
- console.log(`Server is running on port ${PORT}`);
+  logger.info(`Server is running on port ${PORT}`);
 });
