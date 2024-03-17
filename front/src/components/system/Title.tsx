5dd48251f603c27d169c9a5e7ec2bc62ef2c4802
Lucas Wolschick <6264382+LucasWolschick@users.noreply.github.com>
2024-03-17 15:12:05 -0300

    conserta redirect quebrado

--- front/src/components/system/Title.tsx
+++ front/src/components/system/Title.tsx
@@ -1,17 +1,18 @@
 import { faChevronLeft } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import Link from "next/link";
 
 // Component used for the title of the page, since all the pages has the same type style of title
 // So we have the props that contains the title that you want
 export default function Title(props: TitleProps) {
     return (
         <>
-            <a
+            <Link
                 className="text-warning text-decoration-none d-flex align-items-center"
-                href="/"
+                href="/system"
             >
                 <FontAwesomeIcon icon={faChevronLeft} size="2x" /> Voltar
-            </a>
+            </Link>
             <h1 className="text-warning">{props.title}</h1>
         </>
     );
