695b68902bbfc52d6ce9af0a837605c3e9091740
Lucas Wolschick <6264382+LucasWolschick@users.noreply.github.com>
2024-03-17 01:29:33 -0300

    add rotas usuário no cliente

--- front/src/apis/usuarioAPI.tsx
+++ front/src/apis/usuarioAPI.tsx
@@ -1,6 +1,20 @@
 import { api } from "./configs/axiosConfigs";
 
 export const UsuarioAPI = {
+    register: async function (nome: string, email: string, senha: string, ra?: string, matricula?: string) {
+        return await api.request({
+            url: `/api/user/register`,
+            method: "POST",
+            data: {
+                nome,
+                email,
+                senha,
+                ra,
+                matricula
+            }
+        });
+    },
+
     login: async function (email: string, senha: string, remember: boolean) {
         return await api.request({
             url: `/api/user/login`,
@@ -10,6 +24,13 @@ export const UsuarioAPI = {
                 password: senha,
                 remember
             }
+        });
+    },
+
+    confirmEmail: async function (token: string) {
+        return await api.request({
+            url: `/api/user/confirm/${token}`,
+            method: "POST",
         });
     },
 
@@ -31,5 +52,33 @@ export const UsuarioAPI = {
                 password: senha
             }
         });
-    }
+    },
+
+    getLoggedUser: async function (token: string) {
+        return await api.request({
+            url: `/api/user/me`,
+            method: "GET",
+        });
+    },
+
+    deleteUser: async function (id: number) {
+        return await api.request({
+            url: `/api/user/${id}`,
+            method: "DELETE",
+        });
+    },
+
+    getUserById: async function (id: number) {
+        return await api.request({
+            url: `/api/user/${id}`,
+            method: "GET",
+        });
+    },
+
+    getAllUsers: async function () {
+        return await api.request({
+            url: `/api/user`,
+            method: "GET",
+        });
+    },
 };
